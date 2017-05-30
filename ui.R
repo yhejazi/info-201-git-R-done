10a4236bcbe853b265cacd683ac353687dbb7a28
juliazartn <julia.zartn@gmail.com>
2017-05-29 21:43:34 -0700

    Modified ui.R with basic tab structure with word cloud tab

--- ui.R
+++ ui.R
@@ -1,28 +1,29 @@
 library(shiny)
 library(plotly)
 
-shinyUI(fluidPage(
-  
-  titlePanel("Yelp stuff"),
-  
-  sidebarLayout(
-    
-    sidebarPanel(
-      # Create a dropdown menu for different nutrients
-      # selectInput(inputId = "select", label = "Nutrient", choices = c("Fiber", "Carbohydrate", "Sugar"), selected = "Fiber"),
+shinyUI(navbarPage('INFO 201 Project',
+                   tabPanel('Word Cloud',
+                            titlePanel("Yelp stuff"),
       
-      # Create radio buttons for different manufacturers
-      radioButtons(inputId = "radio", label = "Food Categories",
-             choices = list("Chinese" = "chinese", "Japanese" = "japanese", "Korean" = "korean"),
-             selected = "chinese")
-      
-      
-    ),
-    # Plots the data and writes a description of the page.
-    mainPanel(
-      plotOutput("wordcloud")
-
-    )
-  )
-  )
+                            sidebarLayout(
+                              
+                              sidebarPanel(
+                                # Create a dropdown menu for different nutrients
+                                # selectInput(inputId = "select", label = "Nutrient", choices = c("Fiber", "Carbohydrate", "Sugar"), selected = "Fiber"),
+                                
+                                # Create radio buttons for different manufacturers
+                                radioButtons(inputId = "radio", label = "Food Categories",
+                                       choices = list("Chinese" = "chinese", "Japanese" = "japanese", "Korean" = "korean"),
+                                       selected = "chinese")
+                                
+                                
+                              ),
+                              # Plots the data and writes a description of the page.
+                              mainPanel(
+                                plotOutput("wordcloud")
+                          
+                              )
+                            )
+                          )
+                )
 )
