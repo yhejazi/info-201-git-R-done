73865494b39f3bd93d5eb3f06370453b17153a3d
kuo22 <kuohong22@gmail.com>
2017-05-31 01:54:44 -0700

    Word cloud now reads from .csv stored locally

--- ui.R
+++ ui.R
@@ -14,12 +14,10 @@ shinyUI(fluidPage(theme = shinytheme("united"),
                             sidebarLayout(
                               
                               sidebarPanel(
-                                # Create a dropdown menu for different nutrients
-                                # selectInput(inputId = "select", label = "Nutrient", choices = c("Fiber", "Carbohydrate", "Sugar"), selected = "Fiber"),
-                                
-                                # Create radio buttons for different manufacturers
                                 radioButtons(inputId = "radio", label = "Food Categories",
-                                             choices = list("Chinese" = "chinese", "Japanese" = "japanese", "Korean" = "korean"),
+                                             choices = list("Chinese" = "chinese", "Japanese" = "japanese", "Korean" = "korean", "Thai" = "thai",
+                                                            "Mediterranean" = "mediterranean", "Indian" = "indpak", "Mexican" = "mexican",
+                                                            "American (Traditional)" = "tradamerican"),
                                              selected = "chinese")
                                 
                                 
